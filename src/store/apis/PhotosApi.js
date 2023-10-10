304d56ad1dec79e708ecda9e27e857d5873f61f8
Iva Holešinská <iva.holesinska@mailship.com>
2023-10-10 11:21:57 +0200

    Code comments & update the README file

--- src/store/apis/PhotosApi.js
+++ src/store/apis/PhotosApi.js
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { faker } from '@faker-js/faker'
 
+// Redux Toolkit Query
+
 const photosApi = createApi({
   reducerPath: 'photos',
   baseQuery: fetchBaseQuery({
